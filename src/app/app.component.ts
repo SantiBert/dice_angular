31f15a6a2552b52e8ef7d86aca8bd552efa8e46c
Santiago Bertero <sbertero@crystalis-ar.local>
2021-06-12 23:50:16 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AppData';
+  leftDice = '../assets/img/dice2.png';
+  rightDice = '../assets/img/dice1.png';
+  number1:number= 0;
+  number2:number= 0;
+
+  rollDice():void {
+    this.number1 = Math.round((Math.random() * 5) + 1);
+    this.number2 = Math.round((Math.random() * 5) + 1);
+    this.leftDice = '../assets/img/dice' + this.number1 +'.png';
+    this.rightDice = '../assets/img/dice' + this.number2 +'.png';
+  }
+
 }
